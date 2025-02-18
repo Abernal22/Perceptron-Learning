e395972b1df225136dac4d9a982b2f03111e2a0b
Danny Metcalfe <danny5783699@unm.edu>
2025-02-18 14:42:02 -0700

    Classification graphs

--- adalineSGD.py
+++ adalineSGD.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import datasets
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 class AdalineSGD:
     """ADAptive LInear NEuron classifier.
@@ -181,6 +182,31 @@ class AdalineSGD:
         """Return class label after unit step"""
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
     
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+ # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+ # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.figure()
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                 y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=f'Class {cl}',
+                    edgecolor='black')    
 
 if __name__ == "__main__":
     # Load Iris dataset
@@ -226,4 +252,33 @@ if __name__ == "__main__":
     plt.xlabel('Epochs')
     plt.ylabel('Mean Squared Error')
     plt.title('AdalineGD Loss Over Epochs')
-    plt.show()                  
+
+    
+    adaline = AdalineSGD(eta=eta, n_iter=n_iter, random_state=1)
+    adaline.fit(X[:,[0,2]], y)
+    plot_decision_regions(X[:, [0,2]], y, classifier=adaline)
+    plt.xlabel('Sepal length [cm]')
+    plt.ylabel('Petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.title("AdalineSGD classification")
+
+    adaline = AdalineSGD(eta=eta, n_iter=n_iter, random_state=1)
+    adaline.fit_mini_batch_SGD(X[:,[0,2]], y, 3)
+    plot_decision_regions(X[:, [0,2]], y, classifier=adaline)
+    plt.xlabel('Sepal length [cm]')
+    plt.ylabel('Petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.title("AdalineSGD mini batch classification")
+
+    adaline = AdalineSGD(eta=eta, n_iter=n_iter, random_state=1)
+    adaline.fit_mini_batch_SGD(X[:,[0,2]], y, X.shape[0])
+    plot_decision_regions(X[:, [0,2]], y, classifier=adaline)
+    plt.xlabel('Sepal length [cm]')
+    plt.ylabel('Petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.title("AdalineGD classification")
+    plt.show()
+
+
+
+                 
